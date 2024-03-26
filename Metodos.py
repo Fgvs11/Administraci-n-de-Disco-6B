724b9620807759895a9c7b00b72f32aa51b306f9
AlexEP11 <aleeps2911@gmail.com>
2024-03-25 23:21:13 -0600

    Integracion de LOOK y CLOOK

--- Metodos.py
+++ Metodos.py
@@ -17,7 +17,65 @@ def SCAN(nCilindros, peticiones, cilindroInicial):
     pass
 def CSCAN(nCilindros, peticiones, cilindroInicial):
     pass
+
+
+
 def LOOK(nCilindros, peticiones, cilindroInicial):
-    pass
+    peticiones.sort()
+    cilindroActual = cilindroInicial
+    cilindrosAbajo = []
+    cilindrosArriba = []
+    sTiempoEspera =0
+    tiempoEspera = 0
+    desp = 0
+
+    while peticiones:
+        for peticion in peticiones[:]:
+            if (peticion <= cilindroInicial):
+                cilindrosAbajo.append(peticion)
+                peticiones.remove(peticion)
+            elif (peticion > cilindroInicial):
+                cilindrosArriba.append(peticion)
+                peticiones.remove(peticion)
+
+    cilindrosAbajo.sort(reverse=True)
+    cilindrosOrdenados = cilindrosAbajo + cilindrosArriba
+
+    for cilindro in cilindrosOrdenados:
+        tiempoEspera += desp
+        sTiempoEspera += tiempoEspera
+        desp = abs(cilindro - cilindroActual)
+        u.imprimirRenglon(cilindroActual, cilindro, tiempoEspera, desp)
+        cilindroActual = cilindro
+    return [tiempoEspera + desp, sTiempoEspera / len(cilindrosOrdenados)]
+
+
 def CLOOK(nCilindros, peticiones, cilindroInicial):
-    pass
+    peticiones.sort()
+    cilindroActual = cilindroInicial
+    cilindrosAbajo = []
+    cilindrosArriba = []
+    sTiempoEspera =0
+    tiempoEspera = 0
+    desp = 0
+
+    while peticiones:
+        for peticion in peticiones[:]:
+            if (peticion <= cilindroInicial):
+                cilindrosAbajo.append(peticion)
+                peticiones.remove(peticion)
+            elif (peticion > cilindroInicial):
+                cilindrosArriba.append(peticion)
+                peticiones.remove(peticion)
+
+    cilindrosAbajo.sort(reverse=True)
+    cilindrosArriba.sort(reverse=True)
+    cilindrosOrdenados = cilindrosAbajo + cilindrosArriba
+
+    for cilindro in cilindrosOrdenados:
+        tiempoEspera += desp
+        sTiempoEspera += tiempoEspera
+        desp = abs(cilindro - cilindroActual)
+        u.imprimirRenglon(cilindroActual, cilindro, tiempoEspera, desp)
+        cilindroActual = cilindro
+    return [tiempoEspera + desp, sTiempoEspera / len(cilindrosOrdenados)]
