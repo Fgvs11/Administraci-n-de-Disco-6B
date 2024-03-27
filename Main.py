3bf4df777441f62d192eb41711389a1949167450
Fgvs-PC <fgvizcainos@gmail.com>
2024-03-26 20:11:54 -0600

    FCFS

--- Main.py
+++ Main.py
@@ -4,25 +4,29 @@ import utilidades as u
 
 
 nCilindros = v.validar(input('Ingrese el numero de Cilindros[0-199]: '))
-nPeticiones = v.validar(input('Ingrese el numero de peticiones[1-infinito]: '), li = 1, ls = 0)
+nPeticiones = v.validar(
+    input('Ingrese el numero de peticiones[1-infinito]: '), li=1, ls=0)
 peticiones = []
 
 for i in range(nPeticiones):
-    peticiones.append(v.validar(input(f'Ingrese la peticion {i + 1}: '),ls=nCilindros))
+    peticiones.append(
+        v.validar(input(f'Ingrese la peticion {i + 1}: '), ls=nCilindros))
 
-cilindroInicial = v.validar(input(f'Ingrese el cilindro inicial[0-{nCilindros}]: '),ls=nCilindros)
+cilindroInicial = v.validar(
+    input(f'Ingrese el cilindro inicial[0-{nCilindros}]: '), ls=nCilindros)
 
 u.borrarPantalla()
 u.imprimirDatosDeEntrada(nCilindros, peticiones, cilindroInicial)
 seleccion = u.imprimirMenu()
 
+bDir = v.validarBit(input('Ingrese el bit de direccion[0-1]: '))
 u.imprimirCabezera(seleccion)
 if seleccion == 0:
     resultados = m.FCFS(nCilindros, peticiones, cilindroInicial)
 elif seleccion == 1:
     resultados = m.SSTF(nCilindros, peticiones, cilindroInicial)
 elif seleccion == 2:
-    resultados = m.SCAN(nCilindros, peticiones, cilindroInicial)
+    resultados = m.SCAN(nCilindros, peticiones, cilindroInicial, bDir)
 elif seleccion == 3:
     resultados = m.CSCAN(nCilindros, peticiones, cilindroInicial)
 elif seleccion == 4:
@@ -31,8 +35,3 @@ else:
     resultados = m.CLOOK(nCilindros, peticiones, cilindroInicial)
 u.imprimirFinal()
 u.imprimirResultados(resultados)
-
-
-
-
-
--- Validaciones.py
+++ Validaciones.py
@@ -1,10 +1,12 @@
-def __validarRango(num,li,ls):
+def __validarRango(num, li, ls):
     if li <= num and (ls == 0 or num <= ls):
         return num
     else:
-        num = input(f'Error, ingrese un numero entre el rango[{li}-{ls if ls != 0 else "infinito"}]: ')
-        num = validar(num, li = li, ls = ls)
+        num = input(
+            f'Error, ingrese un numero entre el rango[{li}-{ls if ls != 0 else "infinito"}]: ')
+        num = validar(num, li=li, ls=ls)
         return num
+
 
 def __validarNumero(num):
     try:
@@ -15,6 +17,16 @@ def __validarNumero(num):
         num = validar(num)
         return num
 
-def validar(num, li = 0, ls = 199):
+
+def validar(num, li=0, ls=199):
     num = __validarNumero(num)
     return __validarRango(num, li, ls)
+
+
+def validarBit(num):
+    num = __validarNumero(num)
+    if num == 0 or num == 1:
+        return (num)
+    else:
+        print("El número debe ser 0 o 1. Inténtelo de nuevo.")
+        return validarBit(input('Ingrese el bit de dirección[0-1]: '))
