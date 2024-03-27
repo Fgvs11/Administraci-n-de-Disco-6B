5abaee08152b495729b98afed3a202686f19644a
ByCato032 <carlos_eduardo2003@hotmail.es>
2024-03-26 19:07:41 -0600

    validacion bit

--- Main.py
+++ Main.py
@@ -4,13 +4,16 @@ import utilidades as u
 
 
 nCilindros = v.validar(input('Ingrese el numero de Cilindros[0-199]: '))
-nPeticiones = v.validar(input('Ingrese el numero de peticiones[1-infinito]: '), li = 1, ls = 0)
+nPeticiones = v.validar(
+    input('Ingrese el numero de peticiones[1-infinito]: '), li=1, ls=0)
 peticiones = []
 
 for i in range(nPeticiones):
-    peticiones.append(v.validar(input(f'Ingrese la peticion {i + 1}: '),ls=nCilindros))
+    peticiones.append(
+        v.validar(input(f'Ingrese la peticion {i + 1}: '), ls=nCilindros))
 
-cilindroInicial = v.validar(input(f'Ingrese el cilindro inicial[0-{nCilindros}]: '),ls=nCilindros)
+cilindroInicial = v.validar(
+    input(f'Ingrese el cilindro inicial[0-{nCilindros}]: '), ls=nCilindros)
 
 u.borrarPantalla()
 u.imprimirDatosDeEntrada(nCilindros, peticiones, cilindroInicial)
@@ -22,7 +25,8 @@ if seleccion == 0:
 elif seleccion == 1:
     resultados = m.SSTF(nCilindros, peticiones, cilindroInicial)
 elif seleccion == 2:
-    resultados = m.SCAN(nCilindros, peticiones, cilindroInicial)
+    bDir = v.validarBit(input('Ingrese el bit de direccion[0-1]: '))
+    resultados = m.SCAN(nCilindros, peticiones, cilindroInicial, bDir)
 elif seleccion == 3:
     resultados = m.CSCAN(nCilindros, peticiones, cilindroInicial)
 elif seleccion == 4:
@@ -31,8 +35,3 @@ else:
     resultados = m.CLOOK(nCilindros, peticiones, cilindroInicial)
 u.imprimirFinal()
 u.imprimirResultados(resultados)
-
-
-
-
-
--- Metodos.py
+++ Metodos.py
@@ -1,23 +1,62 @@
 import utilidades as u
+import Validaciones as v
+
+
 def FCFS(nCilindros, peticiones, cilindroActual):
-    sTiempoEspera =0
+    sTiempoEspera = 0
     tiempoEspera = 0
     desp = 0
     for cilindroSolicitado in peticiones:
         tiempoEspera += desp
         sTiempoEspera += tiempoEspera
         desp = abs(cilindroSolicitado - cilindroActual)
-        u.imprimirRenglon(cilindroActual, cilindroSolicitado, tiempoEspera, desp)
+        u.imprimirRenglon(cilindroActual, cilindroSolicitado,
+                          tiempoEspera, desp)
         cilindroActual = cilindroSolicitado
     return [tiempoEspera + desp, sTiempoEspera / len(peticiones)]
 
+
 def SSTF(nCilindros, peticiones, cilindroInicial):
     pass
-def SCAN(nCilindros, peticiones, cilindroInicial):
-    pass
+
+
+def SCAN(nCilindros, peticiones, cilindroInicial, bDir):
+    peticiones.sort()
+    cilindroActual = cilindroInicial
+    cilindrosAbajo = []
+    cilindrosArriba = []
+    sTiempoEspera = 0
+    tiempoEspera = 0
+    desp = 0
+    if bDir == '0':
+        while peticiones:
+            for peticion in peticiones[:]:
+                if (peticion <= cilindroInicial):
+                    cilindrosAbajo.append(peticion)
+                    peticiones.remove(peticion)
+                elif (peticion > cilindroInicial):
+                    cilindrosArriba.append(peticion)
+                    peticiones.remove(peticion)
+
+        cilindrosAbajo.sort(reverse=True)
+        cilindrosOrdenados = cilindrosAbajo + cilindrosArriba
+
+        for cilindro in cilindrosOrdenados:
+            tiempoEspera += desp
+            sTiempoEspera += tiempoEspera
+            desp = abs(cilindro - cilindroActual)
+            u.imprimirRenglon(cilindroActual, cilindro, tiempoEspera, desp)
+            cilindroActual = cilindro
+        return [tiempoEspera + desp, sTiempoEspera / len(cilindrosOrdenados)]
+
+
 def CSCAN(nCilindros, peticiones, cilindroInicial):
     pass
+
+
 def LOOK(nCilindros, peticiones, cilindroInicial):
     pass
+
+
 def CLOOK(nCilindros, peticiones, cilindroInicial):
     pass
